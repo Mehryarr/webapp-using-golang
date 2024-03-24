0a60402b0489be190f34443853db7a9eb972d9a2
Mehryarr <mehryarja@gmail.com>
2024-03-24 23:41:22 +0330

    timeout

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func helloWorldWriter(w http.ResponseWriter, r *http.Request) {
@@ -22,11 +23,27 @@ func helloWorldWriter(w http.ResponseWriter, r *http.Request) {
 func htmlVsPlain(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("html vs plain")
 	w.Header().Set("Content-Type", "text/html")
+	//w.Header().Set("Content-Type", "text/plain")
 	fmt.Fprint(w, "<h1>Hello, Welcome!</h1>")
+}
+
+func timeout(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("timeout attempt")
+	time.Sleep(2 * time.Second)
+	fmt.Fprint(w, "did not timeout")
 }
 
 func main() {
 	http.HandleFunc("/", helloWorldWriter)
 	http.HandleFunc("/htmlvsplain", htmlVsPlain)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/timeout", timeout)
+	//log.Fatal(http.ListenAndServe(":8080", nil))
+
+	server := http.Server{
+		Addr:         ":8080",
+		Handler:      nil,
+		ReadTimeout:  1000,
+		WriteTimeout: 1000,
+	}
+	log.Fatal(server.ListenAndServe())
 }
