77680e59182badcd3234bea16b7f494399eaa959
Mehryarr <mehryarja@gmail.com>
2024-03-24 22:27:41 +0330

    html vs plain mode for content type of the header

--- main.go
+++ main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func helloWorldWriter(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World")
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprint(w, "Hello World")
+	case "/myname":
+		fmt.Fprint(w, "Mehryar")
+	default:
+		fmt.Fprint(w, "fatal error")
+	}
+	fmt.Printf("handling function with %s request \n", r.Method)
+
+}
+
+func htmlVsPlain(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("html vs plain")
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Hello, Welcome!</h1>")
 }
 
 func main() {
 	http.HandleFunc("/", helloWorldWriter)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/htmlvsplain", htmlVsPlain)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
